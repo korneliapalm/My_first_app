7c2de286e71078455bdec62ddb54a1481ba4aa86
korneliapalm <31725342+korneliapalm@users.noreply.github.com>
2018-03-28 16:15:42 +0200

    Fixat accelerometern

---
+++ app/src/main/java/com/example/myfirstapp/AccelerometerActivity.java
@@ -0,0 +1,58 @@
+package com.example.myfirstapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView xText;
+    private TextView yText;
+    private TextView zText;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accelerometer);
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        xText = findViewById(R.id.x_axis);
+        yText = findViewById(R.id.y_axis);
+        zText = findViewById(R.id.z_axis);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            xText.setText("" + sensorEvent.values[0]);
+            yText.setText("" + sensorEvent.values[1]);
+            zText.setText("" + sensorEvent.values[2]);
+        }
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        //används inte
+    }
+}
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the accelerometer button */
+    public void accelerometerButton(View view) {
+      Intent intent = new Intent(this, AccelerometerActivity.class);
+      startActivity(intent);
+    }
+
+    /** Called when the user taps the compass button */
+    public void compassButton(View view){
+
+    }
+
+
+
 }
